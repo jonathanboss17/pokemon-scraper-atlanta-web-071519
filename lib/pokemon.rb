b6ccd923e4261f1b7333d0fb90b28a77fa7fafbc
jonathanboss17 <jtboss47@gmail.com>
2019-07-23 15:44:52 -0400

    done all except bonus

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+    attr_accessor :name, :type, :id, :db
+
+    def initialize(props=[])
+        @name = props[1]
+        @type = props[2]
+        @id = props[0]
+    end
+
+    def self.save(name, type, db)
+    sql = "INSERT INTO pokemon (name, type) VALUES (\"#{name}\", \"#{type}\")"
+    db.execute(sql)
+    end
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = #{id} LIMIT 1"
+        x = db.execute(sql)[0]
+        pokemon = Pokemon.new(x)
+        # binding.pry
+        pokemon
+    end
+
 end
+
