d1f77f8590de59ffe388aee2d646179fecdac49c
jonathanboss17 <jtboss47@gmail.com>
2019-07-23 16:29:09 -0400

    done with hashes instead

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,15 @@
 class Pokemon
     attr_accessor :name, :type, :id, :db, :hp
 
-    def initialize(props=[])
-        @name = props[1]
-        @type = props[2]
-        @id = props[0]
-        @hp = props[3]
+    def initialize(props={})
+        @name = props[:name]
+        @type = props[:type]
+        @id = props[:id]
+        @hp = props[:hp]
+        # @name = props[1]
+        # @type = props[2]
+        # @id = props[0]
+        # @hp = props[3]
     end
 
     def self.save(name, type, db)
@@ -16,7 +20,7 @@ class Pokemon
     def self.find(id, db)
         sql = "SELECT * FROM pokemon WHERE id = #{id} LIMIT 1"
         x = db.execute(sql)[0]
-        pokemon = Pokemon.new(x)
+        pokemon = Pokemon.new(name: x[1], id: x[0], type: x[2], hp: x[3])
         pokemon
     end
 
